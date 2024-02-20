5953edadfcd46291043c63813714cd85b6f74328
LorenzoFerrone <l.ferrone@fstechnology.it>
2024-02-20 08:00:22 +0100

    js injections feature added

--- p52/compiler.py
+++ p52/compiler.py
@@ -2,6 +2,7 @@ import time
 import subprocess
 import pathlib
 import re
+import shutil
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -9,39 +10,40 @@ from websockets.sync.client import connect
 
 
 package_dir = pathlib.Path(__file__).parent.resolve()
-# fakeModulesPath = package_dir / "fakeModules"
-realModulesPath = package_dir / "utils"
+importPath = package_dir / "utils"
 
 SOCKET_URI = "ws://localhost:8765"
 
 
-def precompile(sketch):
-    # inject js module into generated html
+def getJSModules(sketch):
+    "return the path of the js modules to inject into the html"
     with open(sketch.path) as src:
         lines = src.readlines()
         injectLines = [l for l in lines if "injectJs" in l]
-        modules = [re.findall("\((.+)\)", l)[0][1:-1] for l in injectLines]
-        modules = [f"../{module}" for module in modules]
-
-    return modules
+        return [re.findall("\((.+)\)", l)[0][1:-1] for l in injectLines]
 
 
 def compileSketch(sketch):
 
-    injections = precompile(sketch)
-    injectedModules = "\n".join([f'<script type="module" src="{module}"></script>' for module in injections])
-
-    # modulesPath = f"{fakeModulesPath}${realModulesPath}"
     outputFolder = sketch.targetFolder / "__target__"
-    command = f"transcrypt -b -n -m -xr -k {sketch.path} -xp {realModulesPath} -od {outputFolder}"
+    command = f"transcrypt -b -n -m -xr -k {sketch.path} -xp {importPath} -od {outputFolder}"
     subprocess.run(command, shell=True)
+
+    # copy js modules inside the new folder and prepare the <src> tags to inject inside index.html
+    modules = getJSModules(sketch)
+    moduleSrcTagsList = []
+    for module in modules:
+        shutil.copy2(sketch.folder / module, sketch.targetFolder)
+        moduleSrcTagsList.append(f'<script src="{module}"></script>')
+
+    modulesSrcTags = "\n".join(moduleSrcTagsList)
 
     with open(package_dir / "static" / "index.html") as inputFile, open(
         sketch.targetFolder / "index.html", "w"
     ) as outputFile:
         index = inputFile.read()
         index = index.replace("{{sketch}}", sketch.namejs)
-        index = index.replace("{{scripts}}", injectedModules)
+        index = index.replace("{{scripts}}", modulesSrcTags)
         outputFile.write(index)
 
 
